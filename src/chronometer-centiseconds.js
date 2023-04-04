646fcd77cb6683cc7944577a530d03330972f07f
Adam Turi <aturike@gmail.com>
2023-04-04 15:04:33 +0200

    done

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,58 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
+    this.counterId = 0;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    this.counterId = setInterval(() => {
+      this.currentTime += 1;
+      printTimeCallback && printTimeCallback();
+    }, 10);
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 6000);
   }
 
   getSeconds() {
     // ... your code goes here
+    return Math.floor((this.currentTime % 6000) / 100);
   }
 
   getCentiseconds() {
     // ... your code goes here
+    return this.currentTime % 100;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return value.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    });
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.counterId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(
+      this.getSeconds()
+    )}.${this.computeTwoDigitNumber(this.getCentiseconds())}`;
   }
 }
--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,55 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+
+    this.currentTime = 0;
+    this.intervalId = null;
+    this.counterId = 0;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+
+    this.counterId = setInterval(() => {
+      this.currentTime += 1;
+      printTimeCallback && printTimeCallback();
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+
+    return value.toLocaleString("en-US", {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    });
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.counterId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
